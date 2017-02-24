5e6df3f8c611965bad040d26fdcfefbb02153eea
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-24 14:04:34 +0200

    tools

--- j24/__init__.py
+++ j24/__init__.py
@@ -1,22 +1,2 @@
 # coding: utf-8
-import os
-
-
-def ensure_dir(directory):
-    """Make sure the directory exists. If not, create it."""
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    return directory
-
-
-def ordinal(n):
-    '''number to ordinal string'''
-    if 10 <= n % 100 < 20:
-        return str(n) + 'th'
-    else:
-        return str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, "th")
-
-
-def home():
-    '''home directory'''
-    return os.path.expanduser('~')
+from j24.tools import ensure_dir, ordinal, home
