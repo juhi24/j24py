dde2476ff9c1cdb7f30b39ecd2f80b3dcf571faa
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-07-03 13:50:05 +0300

    refactor

--- j24/__init__.py
+++ j24/__init__.py
@@ -1,3 +1,4 @@
 # coding: utf-8
 from j24.path import ensure_dir, ensure_join, home
-from j24.tools import ordinal, daterange2str, limitslist
+from j24.datetools import daterange2str, mldatenum2datetime, round_microseconds
+from j24.tools import ordinal, limitslist
--- j24/tools.py
+++ j24/tools.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-
 def limitslist(limits):
     return [(mini, limits[i+1]) for i, mini in enumerate(limits[:-1])]
 
@@ -11,33 +10,3 @@ def ordinal(n):
         return str(n) + 'th'
     else:
         return str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, "th")
-
-
-def daterange2str(start, end=None, dtformat='{day}{month}{year}', delimiter='-',
-          hour_fmt='%H', day_fmt='%d.', month_fmt='%m.', year_fmt='%Y'):
-    """date range in simple human readable format"""
-    formats = {'hour':hour_fmt, 'day':day_fmt, 'month':month_fmt,
-               'year':year_fmt}
-    if end is None:
-        return start.strftime(dtformat.format(**formats))
-    yr_first = dtformat[:6]=='{year}'
-    same_date = start.date() == end.date()
-    start_fmt = dtformat
-    end_fmt = dtformat
-    stripped_fmt = dtformat
-    for attr in ('minute', 'hour', 'day', 'month', 'year'):
-        if getattr(start, attr) == getattr(end, attr) and not same_date:
-            stripped_fmt = stripped_fmt.replace(('{%s}' % attr),'')
-    stripped_fmt = stripped_fmt.strip()
-    if yr_first:
-        end_fmt = stripped_fmt
-    else:
-        start_fmt = stripped_fmt
-    start_str = start.strftime(start_fmt.format(**formats))
-    end_str = end.strftime(end_fmt.format(**formats))
-    if same_date:
-        return start_str
-    return start_str + delimiter + end_str
-
-
-
