d2415fb16406b18e08c82f9aba8388c455a6b727
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-10-30 12:32:59 +0200

    move class coloring logic from radcomp

--- j24/visualization.py
+++ j24/visualization.py
@@ -2,10 +2,48 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
-from j24.tools import shift_edge
+from j24.tools import shift_edge, last_delta
+
+
+DEFAULT_DISCRETE_CMAP = 'tab20'
+
+
+def class_color(cid, cm=None, cmap=DEFAULT_DISCRETE_CMAP, mapping=None,
+                default=(1, 1, 1)):
+    """Pick a color for cid using optional mapping."""
+    cm = cm or plt.get_cmap(cmap)
+    if mapping is not None:
+        if cid in mapping.index:
+            return cm.colors[mapping[cid]]
+        return default
+    return cm.colors[cid]
+
+
+def class_colors(classes, ymin=-0.2, ymax=0, ax=None, alpha=1, mapping=None,
+                 cmap=DEFAULT_DISCRETE_CMAP, displacement_factor=0.5, **kws):
+    """Plot time series of color coding under x axis."""
+    if not isinstance(classes, pd.Series):
+        classes = pd.Series(data=classes, index=classes)
+    if isinstance(classes.index, pd.DatetimeIndex):
+        t = classes.index
+        dt = last_delta(t)*displacement_factor
+        clss = classes.shift(freq=dt).dropna().astype(int)
+    else:
+        clss = classes
+        dt = displacement_factor
+        clss.index = clss.index+dt
+    ax = ax or plt.gca()
+    cm = plt.get_cmap(cmap)
+    t0 = clss.index[0]-2*dt
+    for t1, cid in clss.iteritems():
+        color = class_color(cid, cm, mapping=mapping)
+        ax.axvspan(t0, t1, ymin=ymin, ymax=ymax, facecolor=color,
+                   alpha=alpha, clip_on=False, **kws)
+        t0 = t1
 
 
 def heatmap(t, ax=None, **kws):
