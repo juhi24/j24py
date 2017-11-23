c2de754ab8583133b039c5578ebcd67f668d2e00
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-11-23 16:44:19 +0200

    filename_friendly

--- j24/path.py
+++ j24/path.py
@@ -1,9 +1,12 @@
 # coding: utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
 import os
 
 
 def home():
-    '''home directory'''
+    """home directory"""
     return os.path.expanduser('~')
 
 
@@ -17,3 +20,10 @@ def ensure_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
+
+
+def filename_friendly(namestr, space_replacement='_'):
+    """make string filename friendly"""
+    namestr = namestr.rstrip().replace(' ', space_replacement)
+    keepchars = ('-','.','_')
+    return ''.join(c for c in namestr if c.isalnum() or c in keepchars)
