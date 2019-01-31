fc5a7199c0373cd1e1577e7a609b81ce517fcedc
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-01-31 15:09:12 +0200

    strfdelta

--- j24/datetools.py
+++ j24/datetools.py
@@ -4,6 +4,22 @@ __metaclass__ = type
 
 from datetime import datetime, timedelta
 from copy import deepcopy
+from string import Template
+
+
+class DeltaTemplate(Template):
+    delimiter = "%"
+
+
+def strfdelta(tdelta, fmt):
+    d = {"D": tdelta.days}
+    hours, rem = divmod(tdelta.seconds, 3600)
+    minutes, seconds = divmod(rem, 60)
+    d["H"] = '{:02d}'.format(hours)
+    d["M"] = '{:02d}'.format(minutes)
+    d["S"] = '{:02d}'.format(seconds)
+    t = DeltaTemplate(fmt)
+    return t.substitute(**d)
 
 
 def daterange2str(start, end=None, dtformat='{day}{month}{year}', delimiter='-',
